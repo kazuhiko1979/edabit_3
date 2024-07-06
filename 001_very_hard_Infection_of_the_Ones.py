c3551b24435c9c4a663beea3869c80d63c2de493
kazuhiko1979 <challengevolley1979@gmail.com>
2024-07-06 10:50:32 +0900

    modified py

--- 001_very_hard_Infection_of_the_Ones.py
+++ 001_very_hard_Infection_of_the_Ones.py
@@ -46,17 +46,35 @@ def ones_infection(arr):
     rows = len(arr)
     cols = len(arr[0])
 
-    indices = [(i, j) for i, row in enumerate(arr) for j, _ in enumerate(row)]
-    zero_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+    # 1が存在する行と列を記録
+    infected_rows = set()
+    infected_cols = set()
 
-    for row, col in indices:
-        row_list = arr[row]
-        col_list = [arr[i][col] for i in range(rows)]
+    # 1の位置を特定
+    for i in range(rows):
+        for j in range(cols):
+            if arr[i][j] == 1:
+                infected_rows.add(i)
+                infected_cols.add(j)
 
-        if 1 in row_list or 1 in col_list:
-            zero_matrix[row][col] = 1
+    # 感染した行と列を1で埋める
+    for i in range(rows):
+        for j in range(cols):
+            if i in infected_rows or j in infected_cols:
+                arr[i][j] = 1
+    return arr
 
-    return zero_matrix
+    # indices = [(i, j) for i, row in enumerate(arr) for j, _ in enumerate(row)]
+    # zero_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+    #
+    # for row, col in indices:
+    #     row_list = arr[row]
+    #     col_list = [arr[i][col] for i in range(rows)]
+    #
+    #     if 1 in row_list or 1 in col_list:
+    #         zero_matrix[row][col] = 1
+    #
+    # return zero_matrix
 
 
 
