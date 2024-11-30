8ab07d4737956ed3293387ebd40a93ddf091c0e0
kazuhiko1979 <challengevolley1979@gmail.com>
2024-11-30 11:38:54 +0900

    revised py

--- 026_very_hard_Dividing_into_Chunks_of_Maximum_Sum_N.py
+++ 026_very_hard_Dividing_into_Chunks_of_Maximum_Sum_N.py
@@ -10,31 +10,44 @@ divide([1, 0, 1, 1, -1, 0, 0], 1)
 ➞ [[1, 0], [1], [1, -1, 0, 0]]
 
 divide([2, 1, 0, -1, 0, 0, 2, 1, 3], 3)
-➞ [[2, 1, 0, -1, 0, 0], [2, 1], [3]]
+➞ [[2, 1, 0, -1,: 0, 0], [2, 1], [3]]
 Notes
 The max of the list will always be smaller than or equal to n.
 Use the greedy approach when solving the problem (e.g. fit as many elements you can into a chunk as long as you satisfy the sum constraint)."""
 
 
 def divide(lst, n):
-  answer = []
-  current_chunk = []
-  current_sum = 0
-  
-  for num in lst:
-    if current_sum + num <= n:
-      current_chunk.append(num)
-      current_sum += num
+  res = [[]]
+  for i in lst:
+    if sum(res[-1]) + i > n:
+      res.append([i])
     else:
-      answer.append(current_chunk)
-      current_chunk = [num]
-      current_sum = num
-  
-  if current_chunk:
-    answer.append(current_chunk)
+      res[-1].append(i)
+
+  return res
       
   
-  return answer
+  
+  
+  
+  # answer = []
+  # current_chunk = []
+  # current_sum = 0
+  
+  # for num in lst:
+  #   if current_sum + num <= n:
+  #     current_chunk.append(num)
+  #     current_sum += num
+  #   else:
+  #     answer.append(current_chunk)
+  #     current_chunk = [num]
+  #     current_sum = num
+  
+  # if current_chunk:
+  #   answer.append(current_chunk)
+      
+  
+  # return answer
   
     
   
