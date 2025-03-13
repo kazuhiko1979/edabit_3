8e4875d36757be09fee42f782a005882c23fdc01
kazuhiko1979 <challengevolley1979@gmail.com>
2025-03-13 09:59:16 +0900

    add py

---
+++ 051_very_hard_Substring_Consonant_Vowel_Groups.py
@@ -0,0 +1,66 @@
+"""
+Substring Consonant-Vowel Groups
+Write two functions:
+
+One to retrieve all unique substrings that start and end with a vowel.
+One to retrieve all unique substrings that start and end with a consonant.
+The resulting array should be sorted in lexicographic ascending order (same order as a dictionary).
+
+Examples
+get_vowel_substrings("apple")
+➞ ["a", "apple", "e"]
+
+get_vowel_substrings("hmm") ➞ []
+
+get_consonant_substrings("aviation")
+➞ ["n", "t", "tion", "v", "viat", "viation"]
+
+get_consonant_substrings("motor")
+➞ ["m", "mot", "motor", "r", "t", "tor"]
+Notes
+Remember the output array should have unique values.
+The word itself counts as a potential substring.
+Exclude the empty string when outputting the array.
+"""
+
+def get_vowel_substrings(s):
+    vowels = "aeiou"
+    substrings = set()
+    
+    for i in range(len(s)):
+        if s[i] in vowels:
+            for j in range(i, len(s)):
+                if s[j] in vowels:
+                    substrings.add(s[i:j+1])
+                    
+    return sorted(substrings)
+
+
+def get_consonant_substrings(s):
+    vowels = "aeiou"
+    substrings = set()
+    
+    for i in range(len(s)):
+        if s[i] not in vowels:
+            for j in range(i, len(s)):
+                if s[j] not in vowels:
+                    substrings.add(s[i:j+1])
+                    
+    return sorted(substrings)
+
+
+
+
+print(get_vowel_substrings("apple")) # ["a", "apple", "e"])
+print(get_vowel_substrings("carrot")) # ["a", "arro", "o"])
+print(get_vowel_substrings("aviation")) # ["a", "ati", "atio", "avi", "avia", "aviati", "aviatio", "i", "ia", "iati", "iatio", "io", "o"])
+print(get_vowel_substrings("motor")) # ["o", "oto"])
+print(get_vowel_substrings("rhyme")) # ["e"])
+print(get_vowel_substrings("hmm")) # [])
+
+print(get_consonant_substrings("apple")) # ["l", "p", "pl", "pp", "ppl"])
+print(get_consonant_substrings("carrot")) # ["c", "car", "carr", "carrot", "r", "rot", "rr", "rrot", "t"])
+print(get_consonant_substrings("aviation")) # ["n", "t", "tion", "v", "viat", "viation"])
+print(get_consonant_substrings("motor")) # ["m", "mot", "motor", "r", "t", "tor"])
+print(get_consonant_substrings("rhyme")) # ["h", "hy", "hym", "m", "r", "rh", "rhy", "rhym", "y", "ym"])
+print(get_consonant_substrings("hmm")) # ["h", "hm", "hmm", "m", "mm"])
